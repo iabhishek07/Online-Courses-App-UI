d6838ac30026c117b73a6d1fb3be9dea72d73e28
Abhishek wagh <abhishekwagh@Abhisheks-MacBook-Pro.local>
2020-04-17 16:58:56 +0530

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const kTextColor = Color(0xFF0D1333);
+const kBlueColor = Color(0xFF6E8AFA);
+const kBestSellerColor = Color(0xFFFFD073);
+const kGreenColor = Color(0xFF49CC96);
+
+//Text Styles
+const kHeadingTextStyle = TextStyle(
+  fontSize: 28,
+  color: kTextColor,
+  fontWeight: FontWeight.bold,
+);
+
+const kSubheadingTextStyle = TextStyle(
+  fontSize: 24,
+  color: Color(0xFF61688B),
+  height: 2,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 20,
+  color: kTextColor,
+  fontWeight: FontWeight.bold,
+);
+
+const kSubtitleTextStyle = TextStyle(
+  fontSize: 18,
+  color: kTextColor,
+  // fontWeight: FontWeight.bold
+);
